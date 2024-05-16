e4d77384c2a941b88e039bb2166dd256036dcb23
Alexander Swan <43742263+Swan15@users.noreply.github.com>
2024-05-16 11:53:39 +1000

    loading and not found

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className='flex h-screen w-full items-center justify-center'>
+            <div className='mb-4 h-20 w-20 animate-spin bg-primary-color'></div>
+        </div>
+    )
+}
